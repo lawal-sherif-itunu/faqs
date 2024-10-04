4a13094c57822230875c65f6f44e551bc761646a
lawal-sherif-itunu <63552329+lawal-sherif-itunu@users.noreply.github.com>
2024-10-04 13:10:43 +0100

    workin on the JavaScript
    Added event listeners to the buttons and other JS functions

--- sandbox.js
+++ sandbox.js
@@ -1,15 +1,62 @@
 "use strct";
 
-const button1 = document.querySelector(".questionOne-heading img");
-const plus = document.querySelector(".plus");
-const para1 = document.querySelector(".questionOne p");
+const plus = document.querySelector("img.plus");
+const answer1 = document.querySelector(".questionOne-answer");
+const ones = document.querySelectorAll(".one-remove");
+const twos = document.querySelectorAll(".twos");
+const answer2 = document.querySelector(".questionTwo-answer");
+const plus2 = document.querySelector(".plus2");
+const threes = document.querySelectorAll(".threes");
+const answer3 = document.querySelector(".questionThree-answer");
+const plus3 = document.querySelector(".plus3");
+const fours = document.querySelectorAll(".fours");
+const answer4 = document.querySelector(".questionFour-answer");
+const plus4 = document.querySelector(".plus4");
 
-button1.addEventListener("click", function () {
-  if (para1.style.display === "none") {
-    plus.src = "images/icon-plus.svg";
-  } else if (para1.style.display === "block") {
-    plus.src = "images/icon-minus.svg";
-    para1.style.display = "block";
-  }
-  
+ones.forEach(function (one) {
+  one.addEventListener("click", function () {
+    if (answer1.style.display === "none") {
+      answer1.style.display = "block";
+      plus.src = "images/icon-minus.svg";
+    } else {
+      answer1.style.display = "none";
+      plus.src = "images/icon-plus.svg";
+    }
+  });
+});
+
+twos.forEach(function (two) {
+  two.addEventListener("click", function () {
+    if (answer2.style.display === "none") {
+      answer2.style.display = "block";
+      plus2.src = "images/icon-minus.svg";
+    } else {
+      answer2.style.display = "none";
+      plus2.src = "images/icon-plus.svg";
+    }
+  });
+});
+
+threes.forEach(function (three) {
+  three.addEventListener("click", function () {
+    if (answer3.style.display === "none") {
+      answer3.style.display = "block";
+      plus3.src = "images/icon-minus.svg";
+    } else {
+      answer3.style.display = "none";
+      plus3.src = "images/icon-plus.svg";
+    }
+  });
+});
+
+fours.forEach(function (four) {
+  four.addEventListener("click", function () {
+    if (answer4.style.display === "none") {
+      answer4.style.display = "block";
+      plus4.src = "images/icon-minus.svg";
+    } else {
+      answer4.style.display = "none";
+      plus4.src = "images/icon-plus.svg";
+    }
+  });
 });
